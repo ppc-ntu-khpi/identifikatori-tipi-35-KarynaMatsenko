4b13fb762b7c0c160bb6ebf826e9c6ae57e7dfe0
Karyna <50050449+KarynaMatsenko@users.noreply.github.com>
2020-04-21 18:12:10 +0300

    Add files via upload

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package domain;
 
+/**
+ *
+ * @author Admin
+ */
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+
+    /**
+     * Changes a decimal number to binary
+     * @param R integer
+     * @return the specified number
+     */
+    public static String CalculateBinarySystem(int R){
+        return (String) (Integer.toBinaryString((int) R));
+    }
+    
+    /**
+     * Changes a decimal number to octal
+     * @param R integer 
+     *  @return the specified number in octal notation
+     */
+    public static String CalculateOctalSystem(int R){
+        return (String) (Integer.toOctalString((int) R));
+    }
+    
+    /**
+     * @param R integer 
+     *  @return the specified number in hexadecimal notation
+     */
+    public static String CalculateHexadecimalSystem(int R){
+        return (String) (Integer.toHexString((int) R));
     }
 }
--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,12 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package test;
-
+/**
+ *
+ * @author Admin
+ */
 import domain.Exercise;
 
 public class TestResult {
 
+   
     public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        
+        int R=100000;
+        if(R>=100000 && R<1000000)
+            {
+            System.out.println("Number "+R+" in binary cleaning system: "
+                    + ""+Exercise.CalculateBinarySystem(R)+"\n"
+                    + "in octal calculus: "+Exercise.CalculateOctalSystem(R)
+                    +"\nin hexadecimal number system: "
+                    +Exercise.CalculateHexadecimalSystem(R));
+            }
+        else{
+            System.out.println("The number R must be six-digit");
+            }
     }
 }
